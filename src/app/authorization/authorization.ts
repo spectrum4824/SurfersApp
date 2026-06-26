bb92a05eefd0f3cc6ebfc1da206590259fff144e
ира <irinabelaja4@gmail.com>
2026-06-26 17:55:43 +0400

    2

--- src/app/authorization/authorization.ts
+++ src/app/authorization/authorization.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-authorization',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './authorization.html',
-  styleUrl: './authorization.css',
+  styleUrl: './authorization.css'
 })
-export class Authorization {}
+export class Authorization {
+  nickname: string = '';
+  password: string = '';
+  isPasswordValid: boolean = false;
+
+  constructor(private router: Router) {}
+
+  onPassword() {
+    this.isPasswordValid = this.password.length >= 6;
+  }
+
+  onLogin() {
+    if (!this.nickname || !this.password) {
+      alert('Заполните все поля');
+      return;
+    }
+    if (!this.isPasswordValid) {
+      alert('Пароль должен быть минимум 6 символов');
+      return;
+    }
+    console.log('Никнейм:', this.nickname);
+    console.log('Пароль:', this.password);
+    this.router.navigate(['/feed']);
+  }
+}
--- src/app/registration/registration.ts
+++ src/app/registration/registration.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-registration',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './registration.html',
-  styleUrl: './registration.css',
+  styleUrl: './registration.css'
 })
-export class Registration {}
+export class Registration {
+  nickname: string = '';
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  lastName: string = '';
+  firstName: string = '';
+  contact: string = '';
+  about: string = '';
+  achievements: string = '';
+
+  constructor(private router: Router) {}
+  onPasswordChange() {
+    
+    if (this.confirmPassword.length === 0) {
+      return;
+    }
+  }
+  isPasswordsMatch(): boolean {
+  
+    if (this.confirmPassword.length === 0) {
+      return true;
+    }
+    return this.password === this.confirmPassword;
+  }
+  onRegister() {
+    if (!this.nickname || !this.email || !this.password || !this.confirmPassword) {
+      alert('Заполните все обязательные поля (*)');
+      return;
+    }
+    if (!this.isPasswordsMatch()) {
+      alert('Пароли не совпадают!');
+      return;
+    }
+    if (this.password.length < 6) {
+      alert('Пароль должен быть минимум 6 символов');
+      return;
+    }
+    this.router.navigate(['/feed']);
+  }
+}
