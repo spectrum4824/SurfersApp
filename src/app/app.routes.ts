60ea24be1a2a7f5a3234cb8f7304756b247ad655
ира <irinabelaja4@gmail.com>
2026-06-26 16:31:20 +0400

    1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Newsfeed } from './newsfeed/newsfeed';
 export const routes: Routes = [
   { path: '', component: Newsfeed },
   { path: 'login', component: Authorization },
-  { path: 'registration', component: Registration }
+  { path: 'registration', component: Registration },
+  { path: 'feed', component: Newsfeed }
 ];
 
--- src/app/authorization/authorization.ts
+++ src/app/authorization/authorization.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-authorization',
   imports: [],
   templateUrl: './authorization.html',
